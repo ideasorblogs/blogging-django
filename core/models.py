0f31d39df9e805b17f8ab19bb776d82469d4dc72
agrajhak <agrajhak47@outlook.com>
2022-06-27 20:16:13 +0530

    update

--- core/models.py
+++ core/models.py
@@ -3,4 +3,6 @@ from django.db import models
 from taggit.managers import TaggableManager
 # Create your models here.
 
-
+class newsletter(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
