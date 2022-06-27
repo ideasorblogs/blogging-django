df7527941dfadd43659b26830b8cb8eb3c6fda8b
agrajhak <agrajhak47@outlook.com>
2022-06-27 21:05:33 +0530

    update

--- core/forms.py
+++ core/forms.py
@@ -17,6 +17,7 @@ class NewsletterForm(forms.ModelForm):
             attrs={
                 "type": "email",
                 "autocomplete": "email",
+                "placeholder": "demo@example.com",
                 "class": "flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
             }
         )
