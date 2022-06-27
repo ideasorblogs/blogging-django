4acf0c18e2e307729f66f2597fc8c436d5cdd15e
agrajhak <agrajhak47@outlook.com>
2022-06-27 22:10:20 +0530

    update

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from .views import ProfileView
 from .views import *
 urlpatterns = [
     path('', views.core, name="index"),
-    path('newsletter', views.newsletter, name="newsletter").
+    path('newsletter', views.newsletter, name="newsletter"),
     path('username/<slug:slug>', ProfileView.as_view(), name="profile"),
 ]
