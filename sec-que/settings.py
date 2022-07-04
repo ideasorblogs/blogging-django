1690dddc6b50cf6d8503f751dca09837167ed3e0
agrajhak <agrajhak47@outlook.com>
2022-07-04 12:31:54 +0530

    update

--- sec-que/settings.py
+++ sec-que/settings.py
@@ -292,14 +292,4 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        }
-    }
-}
+SITE_ID = 2
